3df6c6a31574d4f3c7ba6ec8fd3dd173066ee701
rlossec <raphaelossec@gmail.com>
2025-08-25 10:05:45 +0200

    Parse data

--- src/main.py
+++ src/main.py
@@ -1,10 +1,38 @@
 from utils.json import load_json, save_json
 
 DATA_PATH = "../data"
+PRIORITY_ORDER = {
+  "STAT": 0,
+  "URGENT": 1,
+  "ROUTINE": 2
+}
+
+
+def parse_data(data: dict) -> tuple[list, list, list]:
+  samples = data["samples"]
+  technicians = data["technicians"]
+  equipment = data["equipment"]
+  return samples, technicians, equipment
+
 
 def main():
   #Example 1
-  data = load_json(f"{DATA_PATH}/input/tests/example_1.json")
+  data = load_json(f"{DATA_PATH}/input/tests/example_3.json")
+  # Parse data
+  samples, technicians, equipment = parse_data(data)
+  print("samples", samples)
+  print("technicians", technicians)
+  print("equipment", equipment)
+
+  # Sort by priority (STAT, URGENT, ROUTINE)
+
+  # Assign resources
+
+  # Planify
+
+  # Calculate metrics
+
+
   save_json(data, f"{DATA_PATH}/output/tests/results.json")
 
 
